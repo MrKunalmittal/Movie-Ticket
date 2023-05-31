d23f8ca6e483c2e27e9072c7003e7a2c3f89feba
MrKunalmittal <KUNAL>
2023-06-01 00:40:52 +0530

    fixing bugs

--- assests/script/index.js
+++ assests/script/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // importing utility functions
-import { select,print,onEvent } from "./utils.js";
+import { select,print } from "./utils.js";
 
 // basic setup
 const options = { 
@@ -19,7 +19,7 @@ const cityInput = select('.cityInput');
 const cityResults = select('.city-Results');
 const citiesUrl = './assests/script/cities.json';
 
-/*-------------------------city-------------------------- */
+/*------------------------fetching-city-------------------------- */
 
 fetch(citiesUrl)
   .then(response => response.json())
@@ -27,3 +27,114 @@ fetch(citiesUrl)
     const cityNames = data.cities.map(city => city.name);
     autocomplete(cityInput, cityNames);
 });
+
+
+/*------------------------fetching-movie-------------------------- */
+
+fetch(movieUrl)
+  .then(response => response.json())
+  .then(data => {
+   
+    let movieListHTML = '';
+
+    data.movies.forEach(movie => {
+      movieListHTML += `
+        <div class="movie">
+        <img src="${movie.img}" alt="${movie.title}">
+        <h3>${movie.title}  (${(movie.year)})</h3>        
+        </div>
+      `;
+      print(data);
+    });
+
+    movieList.innerHTML = movieListHTML;
+});
+
+//
+cityInput.addEventListener('input', async () => {
+    if (cityInput.value.length > 1) {
+        const inputValue = cityInput.value.toLowerCase();
+  
+        cityResults.innerHTML = '';
+  
+        const response = await fetch(citiesUrl, options);
+        if (!response.ok) {
+            throw new Error(`${response.statusText} (${response.status})`)
+        }
+        const data = await response.json();
+        const cityNames = [];
+        for (let i = 0; i < data.cities.length; i++) {
+            let cityName = data.cities[i].name;
+            cityNames.push(cityName);
+        }
+  
+        const filteredCities = cityNames.filter(city => {
+            return city.toLowerCase().includes(inputValue);
+        })
+  
+        if (filteredCities.length > 0) {
+            filteredCities.forEach(city => {
+                const newElement = document.createElement('a');
+                newElement.href = '#';
+                newElement.textContent = city;
+                newElement.addEventListener('click', () => {
+                    cityInput.value = newElement.textContent;
+                    cityResults.innerHTML = '';
+                })
+                cityResults.appendChild(newElement);
+            })
+        } else {
+            const defaultResult = document.createElement('a');
+            defaultResult.href = '#';
+            defaultResult.textContent = 'City not found';
+            cityResults.appendChild(defaultResult);
+        }
+    } else {
+        cityResults.innerHTML = '';
+    }
+  });
+
+  // movies
+  movieInput.addEventListener('input', async () => {
+    if (movieInput.value.length > 1) {
+        const inputValue = movieInput.value.toLowerCase();
+  
+        movieResults.innerHTML = '';
+  
+        const response = await fetch(movieUrl, options);
+        if (!response.ok) {
+            throw new Error(`${response.statusText} (${response.status})`)
+        }
+        const data = await response.json();
+        const movieTitles = [];
+        for (let i = 0; i < data.movies.length; i++) {
+            let movieName = data.movies[i].title;
+            movieTitles.push(movieName);
+        }
+        
+        const filteredMovies = movieTitles.filter(movie => {
+            return movie.toLowerCase().includes(inputValue);
+        })
+  
+        if (filteredMovies.length > 0) {
+            filteredMovies.forEach(movie => {
+                const newElement = document.createElement('a');
+                newElement.href = '#';
+                newElement.textContent = movie;
+                newElement.addEventListener('click', () => {
+                    movieInput.value = newElement.textContent;
+                    movieResults.innerHTML = '';
+                })
+                movieResults.appendChild(newElement);
+            })
+        } else {
+            const defaultResult = document.createElement('a');
+            defaultResult.href = '#';
+            defaultResult.textContent = 'Movie not found';
+            movieResults.appendChild(defaultResult);
+        }
+    } else {
+        movieResults.innerHTML = '';
+    }
+  });
+  
